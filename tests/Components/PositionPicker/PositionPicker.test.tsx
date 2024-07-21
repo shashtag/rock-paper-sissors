54346dc7503f6f49ce27e1b00658dc590b84c136
Shashwat Gupta <shashwatsatna@gmail.com>
2024-07-21 13:36:54 +0530

    test positionpicker

--- tests/Components/PositionPicker/PositionPicker.test.tsx
+++ tests/Components/PositionPicker/PositionPicker.test.tsx
@@ -1,17 +1,35 @@
 import { render, screen } from "@testing-library/react";
-import { AppProvider } from "../../../src/Global/AppContext";
+import "@testing-library/jest-dom";
 import PositionPicker from "../../../src/Components/PositionPicker/PositionPicker";
+import { POSITIONS } from "../../../src/Global/Constants";
 
-describe("PositionPicker", () => {
-  it("should render", () => {
-    render(
-      <AppProvider>
-        <PositionPicker />
-      </AppProvider>,
-    );
+describe("PositionPicker Component", () => {
+  test("renders the heading", () => {
+    render(<PositionPicker />);
+    const headingElement = screen.getByText(/pick your position/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 
-    const heading = screen.getByText(/pick your position/i);
+  test("renders all positions", () => {
+    render(<PositionPicker />);
+    const positionElements = screen.getAllByTestId("position-item");
+    console.log(positionElements);
+    expect(positionElements).toHaveLength(POSITIONS.length);
+  });
 
-    expect(heading).toBeInTheDocument();
+  test("renders position with correct props", () => {
+    render(<PositionPicker />);
+    screen.debug();
+    POSITIONS.forEach((item) => {
+      const positionElement = screen.getByText(item.position);
+      expect(positionElement).toBeInTheDocument();
+      expect(positionElement.parentElement?.parentElement).toHaveStyle(
+        `background-color: ${item.bg}`,
+      );
+      expect(positionElement.parentElement?.parentElement).toHaveStyle(
+        `border: ${item.border}`,
+      );
+      expect(positionElement).toHaveStyle(`color: ${item.color}`);
+    });
   });
 });
